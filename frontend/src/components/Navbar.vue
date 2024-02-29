6a45826032003e2d50c2706dad43e318389d5e25
NikOverflow <nikoverflow007@gmail.com>
2024-02-29 15:41:53 +0100

    feat(frontend): add routing and remove unnecessary files.
    Signed-off-by: NikOverflow <nikoverflow007@gmail.com>

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <div v-if="$route.path != '/login' && $route.path != '/register'">
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+<style scoped>
+</style>
